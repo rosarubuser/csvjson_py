0412ff10ec5addbf72781b7768783fc8f893fc8a
rosarubuser@gmail.com <rosarubsuer@gmail.com>
2020-10-13 12:04:22 +0800

    refactor the folders and rm the duplicates

--- csv2js.py
+++
@@ -1,9 +0,0 @@
-import csv
-import json
-
-with open("testing.csv", 'r') as inF:
-	dict_f = csv.DictReader(inF)	#read from csv into dictionary
-	with open("result.json", 'w') as outF:
-		for row in dict_f:
-			result = json.dumps(row, indent = 4)	#read from dict to json
-			outF.write(result + '\n')
