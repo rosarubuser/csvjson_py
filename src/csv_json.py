880ee40211f1a9cafe2a3580f0938c2bf9aefa17
rosarubuser@gmail.com <rosarubsuer@gmail.com>
2020-10-14 22:48:26 +0800

    change the function input/output as string; add unit test file

--- src/csv_json.py
+++ src/csv_json.py
@@ -1,46 +1,37 @@
 import csv
 import json
 import argparse
-
-def csv2js(inF_path, outF_path):
-    with open(inF_path, 'r') as inF:
-        dict_f = csv.DictReader(inF, skipinitialspace=True)  # read from csv into dictionary
-        with open(outF_path, 'w') as outF:
-            for row in dict_f:
-                result = json.dumps(row, indent=4)  # read from dict to json
-                outF.write(result + '\n')
+from io import StringIO
 
 
-def js2csv(inF_path, outF_path):
-    with open(inF_path, 'r') as inF:
-        data = inF.read()  # convert io.TextIOWrapper to str
-        dict_f = json.loads(data)  # convert json to list
-        with open(outF_path, 'w') as outF:
-            fieldnames = []  # fieldnames to store keys
-            for key in dict_f[0]:  # for keys in ele0
-                fieldnames.append(key)
-            writer = csv.DictWriter(outF, fieldnames)
-            writer.writeheader()  # add the keys as first row
-            for row in dict_f:  # for each dict in list
-                writer.writerow(row)  # convert dict values to csv
+def csv2js(csv_str):
+    dict_f = csv.DictReader(StringIO(csv_str), skipinitialspace=True)  # read from file into dictionary
+    js_str = ""
+    for row in dict_f:
+        js_str = js_str + json.dumps(row, indent=4) + '\n'  # convert dict to string
+    return js_str
 
 
-''' version 1: input during operation
-inF_path = input("Enter the input file path:")
-outF_path = input("Enter the output file path:")
-choice = input("Convert csv to json[1] or json to csv[2]? (Enter 1 or 2):")
-if choice == '1':  # convert csv to json
-    csv2js(inF_path, outF_path)
-elif choice == '2':  # convert json to csv
-    js2csv(inF_path, outF_path)
-'''
+def js2csv(js_str):
+    list_f = json.loads(js_str)  # convert str to list (which is composed of dicts)
 
-# version 2: utilize the argparse
+    # TODO fix the bug on csv.DictWriter(file required)
+    fieldnames = []  # fieldnames to store keys
+    for key in list_f[0]:  # find the keys
+        fieldnames.append(key)
+    csv_str = ""
+    writer = csv.DictWriter(csv_str, fieldnames)
+    writer.writeheader()  # add the keys as first row
+    for row in list_f:  # for each dict in list
+        writer.writerow(row)  # convert dict values to csv
+    return csv_str
+
 
 ''' for testing input
 inF_path = "../test/testing.csv"
 outF_path = "../test/result.json"
 '''
+
 parser = argparse.ArgumentParser()
 parser.add_argument("input", help="the input file path")
 parser.add_argument("output", help="the output file path")
@@ -48,7 +39,14 @@ parser.add_argument("choice", type=int, choices=[1, 2], help="converting method 
                                                              "1 - from csv to json; "
                                                              "2 - from json to csv")
 args = parser.parse_args()
+
+with open(args.input, 'r') as inF:
+    in_str = inF.read()  # read the input file into string
+
 if args.choice == 1:  # convert csv to json
-    csv2js(args.input, args.output)
+    out_str = csv2js(in_str)
 elif args.choice == 2:  # convert json to csv
-    js2csv(args.input, args.output)
+    out_str = js2csv(in_str)
+
+with open(args.output, 'w') as outF:
+    outF.write(out_str)
---
+++ test/test_csvJson.py
@@ -0,0 +1,7 @@
+# def test_sum():
+#     assert sum((1, 2, 3)) == 5, "Should be 5"
+#
+#
+# if __name__ == "__main__":
+#     test_sum()
+#     print("OK")
