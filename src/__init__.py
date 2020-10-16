a7d8174fe385bb173aaf8a0eafbc3d4d600d1231
rosarubuser@gmail.com <rosarubsuer@gmail.com>
2020-10-16 16:33:07 +0800

    refactor the files; add __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+from src.csv_json import csv2js
+from src.csv_json import js2csv
--- test/test_csvJson.py
+++ test/test_csvJson.py
@@ -1,7 +1,29 @@
-# def test_sum():
-#     assert sum((1, 2, 3)) == 5, "Should be 5"
-#
-#
-# if __name__ == "__main__":
-#     test_sum()
-#     print("OK")
+import unittest
+from ..src import csv2js
+
+
+class TestCsvJson(unittest.TestCase):
+
+    def test_csv2js(self):
+        in_str = "Name, Age, Sex" \
+                 "John, 22, M" \
+                 "Amy, 50, F"
+        result = csv2js(in_str)
+        expected = "{" \
+                   " \"Name\": \"John\"" \
+                   " \"Age\": \"22\"" \
+                   " \"Sex\": \"M\"" \
+                   "}" \
+                   "{" \
+                   " \"Name\": \"Amy\"" \
+                   " \"Age\": \"50\"" \
+                   " \"Sex\": \"F\"" \
+                   "}"
+        self.assertEqual(result == expected, "Should be" + expected)
+
+    def test_js2csv(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
